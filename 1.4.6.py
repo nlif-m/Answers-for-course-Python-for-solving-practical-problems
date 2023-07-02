33eb3eb5c79a300d9ed2c5ccfd3cf9e779fa8224
nlif <m.payunen@gmail.com>
2023-07-02 13:27:37 +0300

    git init

---
+++ 1.4.6.py
@@ -0,0 +1,13 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = "https://stepik.org/media/attachments/lesson/209723/5.html"
+
+html = urlopen(url).read().decode("utf-8")
+s = str(html)
+soup = BeautifulSoup(s, "html.parser")
+
+tds = soup.find_all("td")
+
+
+print(sum([int(i.string) for i in tds]))
